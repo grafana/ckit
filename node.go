grafana/ckit

commit c1fb68e00532b2d11fd33f7d380a133dc0d725d3

ckit: re-gossip state messages
This commit re-gossips state messages received by other peers, ensuring
that they get gossiped and converge across the cluster faster.
Previously, full convergance would only slowly happen over time through
push/pulls if there were more than 3 nodes.

node.go
@@ -393,20 +393,12 @@ func (n *Node) changeState(to peer.State, onDone func()) error {
 
 	// Treat the stateMsg as if it was received externally to track our own state
 	// along with other nodes.
-	n.handleStateMessage(stateMsg)
-
-	bcast, err := messages.Broadcast(&stateMsg, onDone)
-	if err != nil {
-		return err
-	}
-
-	n.broadcasts.QueueBroadcast(bcast)
-	return nil
+	return n.handleStateMessage(stateMsg, onDone)
 }
 
 // handleStateMessage handles a state message from a peer. peerMut must be held
 // when calling.
-func (n *Node) handleStateMessage(msg messages.State) {
+func (n *Node) handleStateMessage(msg messages.State, onDone func()) error {
 	n.clock.Observe(msg.Time)
 
 	n.peerMut.Lock()
@@ -414,8 +406,8 @@ func (n *Node) handleStateMessage(msg messages.State) {
 
 	curr, exist := n.peerStates[msg.NodeName]
 	if exist && msg.Time <= curr.Time {
-		// Ignore a state message if we have a newer one.
-		return
+		// Ignore a state message if we have the same or a newer one.
+		return nil
 	}
 
 	level.Debug(n.log).Log("msg", "handling state message", "msg", msg)
@@ -427,6 +419,14 @@ func (n *Node) handleStateMessage(msg messages.State) {
 		n.peers[msg.NodeName] = p
 		n.handlePeersChanged()
 	}
+
+	// We haven't seen this message before; gossip it to our peers.
+	bcast, err := messages.Broadcast(&msg, onDone)
+	if err != nil {
+		return err
+	}
+	n.broadcasts.QueueBroadcast(bcast)
+	return nil
 }
 
 // Peers returns all Peers currently known by n. The Peers list will include
@@ -550,7 +550,10 @@ func (nd *nodeDelegate) NotifyMsg(raw []byte) {
 			return
 		}
 
-		nd.handleStateMessage(s)
+		// We can ignore errors here, which happen if we failed to rebroadcast the
+		// message. Messages will still eventually converge eventually through
+		// push/pulls.
+		_ = nd.handleStateMessage(s, nil)
 	default:
 		nd.m.gossipEventsTotal.WithLabelValues(eventUnkownMessage).Inc()
 
@@ -651,6 +654,7 @@ func (nd *nodeDelegate) MergeRemoteState(buf []byte, join bool) {
 		// entry for this peer, we can treat it as stale and delete it.
 		_, peerExistsRemote := remoteStates[nodeName]
 		if !peerExistsRemote {
+			level.Debug(nd.log).Log("msg", "deleting stale reference to node", "node", nodeName)
 			delete(nd.peerStates, nodeName)
 		}
 	}

commit 4b262bfda075a10bb2d3fd0130cfb50a6af1852a

ckit: fix deadlock between handling user message and queueing a broadcast
While c1fb68e00532b2d11fd33f7d380a133dc0d725d3 made gossip convergence
happen faster, it introduced the possibility for a deadlock to occur
while handling user messages.

The gossip transmit queue used the NumMembers function of the
memberlist, which grabs a lock on the node state. If UserMsg was called
while the memberlist's node state was locked, we'd deadlock.

I was unable to find the specific code path that caused the deadlock,
but it was at least obvious that c1fb68e introduced it. This commit
changes the transmit queue to load from an atomic field instead, which
is also how memberlist is internally implemented.

node.go
@@ -23,6 +23,7 @@ import (
 	"github.com/rfratto/ckit/internal/queue"
 	"github.com/rfratto/ckit/peer"
 	"github.com/rfratto/ckit/shard"
+	"go.uber.org/atomic"
 	"google.golang.org/grpc"
 )
 
@@ -125,6 +126,11 @@ type Node struct {
 	peerStates map[string]messages.State // State lookup for a node name
 	peers      map[string]peer.Peer      // Current list of peers & their states
 	peerCache  []peer.Peer               // Slice version of peers; keep in sync with peers
+
+	// Rough number of current peers used to gossip messages. Using an atomic
+	// variable avoids deadlocks with queueing broadcasts given a memberlist
+	// event if the broadcast queue used memberlist.Memberlist.NumMembers.
+	estPeerCount atomic.Int32
 }
 
 // NewNode creates an unstarted Node to participulate in a cluster. An error
@@ -188,7 +194,7 @@ func NewNode(srv *grpc.Server, cfg Config) (*Node, error) {
 	}
 
 	n.ml = ml
-	n.broadcasts.NumNodes = ml.NumMembers
+	n.broadcasts.NumNodes = func() int { return int(n.estPeerCount.Load()) }
 	n.broadcasts.RetransmitMult = mlc.RetransmitMult
 
 	// Include some extra metrics.
@@ -472,6 +478,7 @@ func (n *Node) handlePeersChanged() {
 
 	n.peerCache = newPeers
 	n.notifyObserversQueue.Enqueue(newPeers)
+	n.estPeerCount.Store(int32(len(n.peerCache)))
 }
 
 // Observe registers o to be informed when the cluster changes. o will be