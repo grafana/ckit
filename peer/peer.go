8110cc06aa86ff66a924f32414527b0d57e932d4
Paschalis Tsilias <paschalist0@gmail.com>
2023-06-27 14:42:00 +0300

    peer: implement json.Marshaler interface
    Signed-off-by: Paschalis Tsilias <paschalist0@gmail.com>

--- peer/peer.go
+++ peer/peer.go
@@ -1,6 +1,8 @@
 // Package peer describes a ckit peer.
 package peer
 
+import "encoding/json"
+
 // Peer is a discovered node within the cluster.
 type Peer struct {
 	Name  string // Name of the Peer. Unique across the cluster.
@@ -11,3 +13,19 @@ type Peer struct {
 
 // String returns the name of p.
 func (p Peer) String() string { return p.Name }
+
+// MarshalJSON implements json.Marshaler.
+func (p Peer) MarshalJSON() ([]byte, error) {
+	type peerStatusJSON struct {
+		Name  string `json:"name"`   // Name of the Peer. Unique across the cluster.
+		Addr  string `json:"addr"`   // host:port address of the peer.
+		Self  bool   `json:"isSelf"` // True if Peer is the local Node.
+		State string `json:"state"`  // State of the peer.
+	}
+	return json.Marshal(&peerStatusJSON{
+		Name:  p.Name,
+		Addr:  p.Addr,
+		Self:  p.Self,
+		State: p.State.String(),
+	})
+}
