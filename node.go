0e133c5514ce198913dc1ec8fe3a0d3eb4dd769d
Tiago Grimaldi Rossi <tiagogrimaldirossi@gmail.com>
2024-09-11 07:44:55 -0400

    test commit

--- node.go
+++ node.go
@@ -160,6 +160,7 @@ func NewNode(cli *http.Client, cfg Config) (*Node, error) {
 		Log:           cfg.Log,
 		Client:        cli,
 		PacketTimeout: 1 * time.Second,
+		// test
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to build transport: %w", err)
